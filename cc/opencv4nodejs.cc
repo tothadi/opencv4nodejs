dbda04fad2d1ea00802dc8548247cf5652ce0d28
Adam Toth <tothadam.onp@gmail.com>
2020-06-21 12:47:28 +0200

    Update opencv4nodejs.cc

--- cc/opencv4nodejs.cc
+++ cc/opencv4nodejs.cc
@@ -46,95 +46,120 @@
 #include "ximgproc/ximgproc.h"
 #endif
 
-int customCvErrorHandler(int status, const char* func_name, const char* err_msg, const char* file_name, int line, void* userdata) {
-    std::string msg = "OpenCV Error: (" + std::string(err_msg) + ")"
-      + " in " + std::string(func_name)
-      + ", in file " + std::string(file_name)
-      + ", line " + std::to_string(line)
-      + ", status " + std::to_string(status);
+class AddonData
+{
+public:
+    int customCvErrorHandler(int status, const char *func_name, const char *err_msg, const char *file_name, int line, void *userdata)
+    {
+        std::string msg = "OpenCV Error: (" + std::string(err_msg) + ")" + " in " + std::string(func_name) + ", in file " + std::string(file_name) + ", line " + std::to_string(line) + ", status " + std::to_string(status);
 
-    throw std::runtime_error(msg);
-    return 0;
-}
+        throw std::runtime_error(msg);
+        return 0;
+    }
 
-void init(v8::Local<v8::Object> target) {
-	// can be disabled by defining env variable: OPENCV4NODEJS_DISABLE_EXTERNAL_MEM_TRACKING
-	ExternalMemTracking::Init(target);
+    void init(v8::Local<v8::Object> target)
+    {
+        // can be disabled by defining env variable: OPENCV4NODEJS_DISABLE_EXTERNAL_MEM_TRACKING
+        ExternalMemTracking::Init(target);
 
-	// override cv error handler to prevent printing cv errors and throw std::exception
-	// instead, which can be catched and forwarded to node process
-	cv::redirectError(customCvErrorHandler);
+        // override cv error handler to prevent printing cv errors and throw std::exception
+        // instead, which can be catched and forwarded to node process
+        cv::redirectError(customCvErrorHandler);
 
+        node::AddEnvironmentCleanupHook(v8::Local<v8::Object>, DeleteInstance, this);
 
-	v8::Local<v8::Object> version = Nan::New<v8::Object>();
-	Nan::Set(version, FF::newString("major"), Nan::New(CV_VERSION_MAJOR));
-	Nan::Set(version, FF::newString("minor"), Nan::New(CV_VERSION_MINOR));
-	Nan::Set(version, FF::newString("revision"), Nan::New(CV_VERSION_REVISION));
-	Nan::Set(target, FF::newString("version"), version);
+        v8::Local<v8::Object> version = Nan::New<v8::Object>();
+        Nan::Set(version, FF::newString("major"), Nan::New(CV_VERSION_MAJOR));
+        Nan::Set(version, FF::newString("minor"), Nan::New(CV_VERSION_MINOR));
+        Nan::Set(version, FF::newString("revision"), Nan::New(CV_VERSION_REVISION));
+        Nan::Set(target, FF::newString("version"), version);
 
-	v8::Local<v8::Object> modules = Nan::New<v8::Object>();
-	Nan::Set(target, FF::newString("modules"), modules);
+        v8::Local<v8::Object> modules = Nan::New<v8::Object>();
+        Nan::Set(target, FF::newString("modules"), modules);
 
-	Nan::Set(modules, FF::newString("core"), Nan::New(true));
-	Core::Init(target);
+        Nan::Set(modules, FF::newString("core"), Nan::New(true));
+        Core::Init(target);
+        node::AddEnvironmentCleanupHook(target, DeleteInstance, this);
 #ifdef HAVE_OPENCV_CALIB3D
-	Nan::Set(modules, FF::newString("calib3d"), Nan::New(true));
-	Calib3d::Init(target);
+        Nan::Set(modules, FF::newString("calib3d"), Nan::New(true));
+        Calib3d::Init(target);
 #endif
 #ifdef HAVE_OPENCV_DNN
-	Nan::Set(modules, FF::newString("dnn"), Nan::New(true));
-	Dnn::Init(target);
+        Nan::Set(modules, FF::newString("dnn"), Nan::New(true));
+        Dnn::Init(target);
 #endif
 #ifdef HAVE_OPENCV_FACE
-	Nan::Set(modules, FF::newString("face"), Nan::New(true));
-	Face::Init(target);
+        Nan::Set(modules, FF::newString("face"), Nan::New(true));
+        Face::Init(target);
 #endif
 #ifdef HAVE_OPENCV_FEATURES2D
-	Nan::Set(modules, FF::newString("features2d"), Nan::New(true));
-	Features2d::Init(target);
+        Nan::Set(modules, FF::newString("features2d"), Nan::New(true));
+        Features2d::Init(target);
 #endif
 #ifdef HAVE_OPENCV_IMGPROC
-	Nan::Set(modules, FF::newString("imgproc"), Nan::New(true));
-	Imgproc::Init(target);
+        Nan::Set(modules, FF::newString("imgproc"), Nan::New(true));
+        Imgproc::Init(target);
 #endif
 #ifdef HAVE_IO
-	Nan::Set(modules, FF::newString("io"), Nan::New(true));
-	Io::Init(target);
+        Nan::Set(modules, FF::newString("io"), Nan::New(true));
+        Io::Init(target);
 #endif
 #ifdef HAVE_OPENCV_ML
-	Nan::Set(modules, FF::newString("ml"), Nan::New(true));
-	MachineLearning::Init(target);
+        Nan::Set(modules, FF::newString("ml"), Nan::New(true));
+        MachineLearning::Init(target);
 #endif
 #ifdef HAVE_OPENCV_OBJDETECT
-	Nan::Set(modules, FF::newString("objdetect"), Nan::New(true));
-	Objdetect::Init(target);
+        Nan::Set(modules, FF::newString("objdetect"), Nan::New(true));
+        Objdetect::Init(target);
 #endif
 #ifdef HAVE_OPENCV_PHOTO
-	Nan::Set(modules, FF::newString("photo"), Nan::New(true));
-	Photo::Init(target);
+        Nan::Set(modules, FF::newString("photo"), Nan::New(true));
+        Photo::Init(target);
 #endif
 #ifdef HAVE_OPENCV_TEXT
-	Nan::Set(modules, FF::newString("text"), Nan::New(true));
-	Text::Init(target);
+        Nan::Set(modules, FF::newString("text"), Nan::New(true));
+        Text::Init(target);
 #endif
 #ifdef HAVE_OPENCV_TRACKING
-	Nan::Set(modules, FF::newString("tracking"), Nan::New(true));
-	Tracking::Init(target);
+        Nan::Set(modules, FF::newString("tracking"), Nan::New(true));
+        Tracking::Init(target);
 #endif
 #ifdef HAVE_OPENCV_VIDEO
-	Nan::Set(modules, FF::newString("video"), Nan::New(true));
-	Video::Init(target);
+        Nan::Set(modules, FF::newString("video"), Nan::New(true));
+        Video::Init(target);
 #endif
 #ifdef HAVE_OPENCV_XFEATURES2D
-	Nan::Set(modules, FF::newString("xfeatures2d"), Nan::New(true));
-	XFeatures2d::Init(target);
+        Nan::Set(modules, FF::newString("xfeatures2d"), Nan::New(true));
+        XFeatures2d::Init(target);
 #endif
 #ifdef HAVE_OPENCV_XIMGPROC
-	Nan::Set(modules, FF::newString("ximgproc"), Nan::New(true));
-	XImgproc::Init(target);
+        Nan::Set(modules, FF::newString("ximgproc"), Nan::New(true));
+        XImgproc::Init(target);
 #endif
+    };
+
+    static void DeleteInstance(void *data)
+    {
+        delete static_cast<AddonData *>(data);
+    }
 };
 
-NODE_MODULE_INIT() {
-	init();
+static void Method(const v8::FunctionCallbackInfo<v8::Value> &info)
+{
+    // Retrieve the per-addon-instance data.
+    AddonData *data =
+        reinterpret_cast<AddonData *>(info.Data().As<External>()->Value());
+    data->call_count++;
+    info.GetReturnValue().Set((double)data->call_count);
+}
+
+NODE_MODULE_INIT()
+{
+    AddonData *data = new AddonData(target);
+    Local<External> external = External::New(target, data);
+    exports->Set(context,
+               String::NewFromUtf8(target, "method", NewStringType::kNormal)
+                  .ToLocalChecked(),
+               FunctionTemplate::New(target, Method, external)
+                  ->GetFunction(context).ToLocalChecked()).FromJust();
 }
